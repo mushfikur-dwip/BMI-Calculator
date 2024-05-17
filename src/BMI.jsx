041092ff8679d3c5ad30529232915c985f1b3f7b
mushfikur-dwip <musfikurrahmandip@gmail.com>
2024-05-17 18:35:33 +0600

    UI Changed

--- src/BMI.jsx
+++ src/BMI.jsx
@@ -10,25 +10,32 @@ function BMI() {
     <>
         <div className="app">
           <div className="container">
-            <h2>BMI Calculator</h2>
+          <div className='heading'>
+            <h1>BMI Calculator</h1>
+          </div>
             <form>
               <div className='weight'>
                 <label htmlFor="" >Weight (ibs)</label>
-                <input type="text" placeholder='Enter Wight Value' value={weight} />
+                <input type="text" placeholder='Enter Wight Value'  />
               </div>
               <div className='height'>
                 <label htmlFor="">Height (in)</label>
-                <input type="text" placeholder='Enter Wight Value' value={height} />
+                <input type="text" placeholder='Enter Wight Value'  />
               </div>
-              <div>
-                <button className="btn " type='submit' >Submit</button>
-                <button className="btn btn-outline" onClick={reload} type='submit' >Reset</button>
+              <div className='buttons'>
+                <button className="submit_btn" type='submit' >Submit</button>
+                <button className="reload" onClick={reload} type='submit' >Reset</button>
               </div>
-            </form>
+          </form>
+          <div className="msg">
+            <span>Your BMI is <span className='bmi-point'>18.29</span></span>
+            <span>Your are <span className='bmi-msg'>Healthy</span></span>
+          </div>
           </div>
         </div>
     </>
   )
 }
+
 
 export default BMI
